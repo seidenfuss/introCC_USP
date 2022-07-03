63ce7c94cf445fabffc41d3e57fea698a40b9707
seidenfuss <seidenfussbender@gmail.com>
2022-07-03 20:06:52 -0300

    week5

--- week5/test_sample.py
+++ week5/test_sample.py
@@ -4,4 +4,8 @@ def func(x):
 
 
 def test_answer():
-    assert func(3) == 5
+    assert func(3) == 5 #verifique se...
+
+#para rodar o teste basta digitar pytest no terminal;
+# Arquivos do tipo test_*.py ou *_test.py indicam que 
+# se trata de um teste
